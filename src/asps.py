1bda0936c6951f4049ffc8469861e3ded1288830
dennis <dennis_kleber@mailbox.org>
2022-02-11 10:29:27 +0100

    update

--- src/asps.py
+++ src/asps.py
@@ -77,36 +77,36 @@ class Asps:
     
 
 
-    def set(self, email, crew, pillar):
-        presentDate = datetime.datetime.now()
-        unix_timestamp = datetime.datetime.timestamp(presentDate)*1000
-        with self.connection.cursor() as cursor:
-            sql = ('select s.id from Profile as p left join Supporter as s on s.profile_id = p.id '
-                    'where LOWER(email) = %s')
-            cursor.execute(sql, (email.lower(),))
-            result = cursor.fetchone()
-            if result != None:
-                s_id = result["id"]
-            else:
-                raise Exception(email + " -- No Profile with given email ")
-            sql = ('select c.id from Crew as c where c.name = %s')
-            cursor.execute(sql, (crew,))
-            result = cursor.fetchone()
-            if result != None:
-                c_id = result["id"]
-            else:
-                raise Exception("No Crew with given crew " + crew)
-
-        with self.connection.cursor() as cursor:
-            # Create a new record
-            sql = ("insert into Supporter_Crew " 
-                    "(`supporter_id`, `crew_id`, `role`, `pillar`, `updated`, `created`, `active`, `nvm_date` )"
-                    " VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
-            cursor.execute(sql, (s_id, c_id, "VolunteerManager", pillar, unix_timestamp, unix_timestamp, None, None,  ))
-
-        # connection is not autocommit by default. So you must commit to save
-        # your changes.
-        self.connection.commit()
+##    def set(self, email, crew, pillar):
+#        presentDate = datetime.datetime.now()
+#        unix_timestamp = datetime.datetime.timestamp(presentDate)*1000
+#        with self.connection.cursor() as cursor:
+#            sql = ('select s.id from Profile as p left join Supporter as s on s.profile_id = p.id '
+#                    'where LOWER(email) = %s')
+#            cursor.execute(sql, (email.lower(),))
+#            result = cursor.fetchone()
+#            if result != None:
+#                s_id = result["id"]
+#            else:
+#                raise Exception(email + " -- No Profile with given email ")
+#            sql = ('select c.id from Crew as c where c.name = %s')
+#            cursor.execute(sql, (crew,))
+#            result = cursor.fetchone()
+#            if result != None:
+#                c_id = result["id"]
+#            else:
+#                raise Exception("No Crew with given crew " + crew)
+#
+#        with self.connection.cursor() as cursor:
+#            # Create a new record
+#            sql = ("insert into Supporter_Crew " 
+#                    "(`supporter_id`, `crew_id`, `role`, `pillar`, `updated`, `created`, `active`, `nvm_date` )"
+#                    " VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+#            cursor.execute(sql, (s_id, c_id, "VolunteerManager", pillar, unix_timestamp, unix_timestamp, None, None,  ))
+#
+#        # connection is not autocommit by default. So you must commit to save
+#        # your changes.
+#        self.connection.commit()
     
     
     
@@ -125,32 +125,39 @@ class Asps:
                 email_list.append(e["email"])
             email_list = list(set(email_list))
             for email in email_list:
-                self.deleteOrUpdate(email)
+                self.delete(email)
 
 
 
-    def deleteOrUpdate(self, email):
+
+
+
+    def set(self, email, crew, pillar):
+        with self.connection.cursor() as cursor:
+            sql = ('select s.id from Profile as p left join Supporter as s on s.profile_id = p.id '
+                    'where LOWER(email) = %s')
+            cursor.execute(sql, (email.lower(),))
+            result = cursor.fetchone()
+            if result == None:
+                raise Exception(email + " -- No Profile with given email ")
+        print("update " + email)
         if email != None:
-            where = 'where LOWER(p.email) = ' + '"' + email + '"'
+            where = 'where LOWER(p.email) = ' + '"' + email + '" ' +'and c.name = "' + crew + '"'
         else:
             print("error")
             exit(1)
-
         with self.connection.cursor() as cursor:
-            sql = ('select p.email, s.full_name, c.name, sc.role, sc.pillar, sc.updated, sc.created '
-                'from Profile as p '
-                'left join Supporter as s on p.id = s.profile_id '
-                'left join Supporter_Crew as sc on sc.supporter_id = s.id '
-                'left join Crew as c on c.id = sc.crew_id ' + where )
-            cursor.execute(sql)
-            result = cursor.fetchall()
-        if len(result) == 1:
-            self.update(email)
-        else:
-            self.delete(email)
+            sql = ('update Supporter_Crew as sc '
+                    'left join Supporter as s on sc.supporter_id = s.id '
+                    'left join Profile as p on s.profile_id = p.id '
+                    'left join Crew as c on c.id = sc.crew_id ' 
+                    'set sc.pillar = %s, sc.role = "VolunteerManager" '
+                    + where
+                )
+            cursor.execute(sql, pillar)
+        self.connection.commit()
 
-
-    def update(self, email):
+    def delete(self, email):
         print("update " + email)
         if email != None:
             where = 'where LOWER(p.email) = ' + '"' + email + '" and sc.role = "VolunteerManager"'
@@ -163,24 +170,6 @@ class Asps:
                     'left join Profile as p on s.profile_id = p.id '
                     'left join Crew as c on c.id = sc.crew_id ' 
                     'set sc.pillar = NULL, sc.role = NULL '
-                    + where
-                )
-            cursor.execute(sql)
-        self.connection.commit()
-
-    def delete(self, email):
-        print("delete " + email)
-        if email != None:
-            where = 'where LOWER(p.email) = ' + '"' + email + '" and sc.role = "VolunteerManager"'
-        else:
-            print("error")
-            exit(1)
-        with self.connection.cursor() as cursor:
-            sql = ('delete sc '
-                    'from Supporter_Crew as sc '
-                    'left join Supporter as s on sc.supporter_id = s.id '
-                    'left join Profile as p on s.profile_id = p.id '
-                    'left join Crew as c on c.id = sc.crew_id ' 
                     + where
                 )
             cursor.execute(sql)
